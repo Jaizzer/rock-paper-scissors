9f6ba562314376f77c0e830387370154762d3e56
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-04-22 08:45:27 +0800

    Add expression to select which message to display in final-result

--- javascript.js
+++ javascript.js
@@ -77,7 +77,7 @@ function playRound(event) {
 
     // Display who got to 5 first.
     if (userScore === 5 || computerScore === 5) {
-        console.log(userScore === 5 ? "You reached 5!" : "Computer reached 5!");
+        finalResult.textContent = (userScore === 5) ? "You Won" : "Computer Won!";
         userScore = 0;
         computerScore = 0;
     }
